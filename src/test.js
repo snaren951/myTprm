206e44584c716e335292a4c32bae653652d6ca75
Naren <naren@mac.home>
2025-10-23 03:09:26 -0400

    added timeouts

--- src/test.js
+++ src/test.js
@@ -1,2 +1,12 @@
 
-console.log("Test File");
+
+console.log("Hello");
+setTimeout(()=>{
+    console.log("1st TimeOut")
+},1000);
+console.log("How are you");
+setTimeout(()=>{
+    console.log("2nd TimeOut")
+},1000);
+
+
