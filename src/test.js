6475b6416df84263716286c23d3679a1ef2e755b
Naren <naren@mac.home>
2025-10-23 03:13:21 -0400

    added set Immediate

--- src/test.js
+++ src/test.js
@@ -3,8 +3,10 @@
 console.log("Hello");
 setTimeout(()=>{
     console.log("1st TimeOut")
-},1000);
+},0);
 console.log("How are you");
+setImmediate(()=> console.log("This is from Set Immediate"));
+console.log("That is nice to know");
 setTimeout(()=>{
     console.log("2nd TimeOut")
 },1000);
